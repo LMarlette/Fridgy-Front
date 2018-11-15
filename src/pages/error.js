87df8c1ef61238252abfa2f9e4cca65ba392726b
sam-mundle <sam.mundle13@gmail.com>
2018-11-14 20:32:55 -0500

    added routing to app.js & added standup pages as routes

---
+++ src/pages/error.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+  
+ class SignUp extends Component {
+  render() {
+    return (
+         <div>
+            <p>Sorry Page doesn't exist!</p>
+
+        </div>
+     );
+  }
+}
+
+export default SignUp;
