d48cd3d10db4b638278782cbb9682b4494372d86
Lilli Jimenez <LilliMJimenez@gmail.com>
2018-11-14 19:49:14 -0500

    frontend page standup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/pages/fridge.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+  
+ class Fridge extends Component {
+  render() {
+    return (
+         <div>
+
+        test Fridge
+
+        </div>
+     );
+  }
+}
+
+export default Fridge;
